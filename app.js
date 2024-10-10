45920b32916593c7eb884159139e462ebf2055dc
dhruv3102 <dhruv.s@keka.com>
2024-10-10 23:28:48 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+//Modules
+//CommonJs, every file is module (by default)
+// Modules -Encapsulated Code (only share minimum )
+
+const exportName = require('./4-name')
+const exportsayHi = require('./5-utlis')
+const data = require('.//6-alternative-flavour');
+require('./7-mind-grenade');
+
+// console.log(exportName);
+// exportsayHi(exportName.dhruv)
+// exportsayHi(exportName.sharma)
+
+// console.log(data)
