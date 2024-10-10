f4e02753e386dfbd4906e70009830e0ea34280e2
Dhruv Sharma <86529130+Dhruv159@users.noreply.github.com>
2024-10-10 23:25:50 +0530

    Update 1-intro.js

--- 1-intro.js
+++ 1-intro.js
@@ -6,3 +6,11 @@ if(amount<10) {
 	console.log('large number');
 }
 console.log('JIJA');
+const amount = 12
+
+if(amount<10) {
+	console.log('small number');
+} else {
+	console.log('large number');
+}
+console.log('JIJA');
