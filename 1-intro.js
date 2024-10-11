1653b027d16d6706a8e51a01c20aea2a72c0f5a0
dhruv159 <dhruvsarkhandia9@gmail.com>
2024-10-11 23:56:44 +0530

    code formatting

--- 1-intro.js
+++ 1-intro.js
@@ -6,11 +6,3 @@ if(amount<10) {
 	console.log('large number');
 }
 console.log('JIJA');
-const amount = 12
-
-if(amount<10) {
-	console.log('small number');
-} else {
-	console.log('large number');
-}
-console.log('JIJA');
