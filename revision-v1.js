46666bd17b3d67dbe832a14e902e0bfc87e42595
Dhruv Sharma <86529130+Dhruv159@users.noreply.github.com>
2024-10-19 17:45:05 +0530

    Update revision-v1.js
    Revised already learned topic

--- revision-v1.js
+++ revision-v1.js
@@ -3,3 +3,21 @@ require('./6-alternative-flavour');
 console.log(name.hello)
 console.log(require('./6-alternative-flavour'));
 console.log(require('./6-alternative-flavour').items)
+
+const name = require('./revision-1-7');
+require('./6-alternative-flavour');
+console.log(name.hello)
+console.log(require('./6-alternative-flavour'));
+console.log(require('./6-alternative-flavour').items)
+
+const name = require('./revision-1-7');
+require('./6-alternative-flavour');
+console.log(name.hello)
+console.log(require('./6-alternative-flavour'));
+console.log(require('./6-alternative-flavour').items)
+
+const name = require('./revision-1-7');
+require('./6-alternative-flavour');
+console.log(name.hello)
+console.log(require('./6-alternative-flavour'));
+console.log(require('./6-alternative-flavour').items)
