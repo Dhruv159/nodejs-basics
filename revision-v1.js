7f6ecbb979fdb3049d9aac82b53027caece75bea
dhruv159 <dhruvsarkhandia9@gmail.com>
2024-10-24 18:12:26 +0530

    updated revision one

--- revision-v1.js
+++ revision-v1.js
@@ -4,14 +4,3 @@ console.log(name.hello)
 console.log(require('./6-alternative-flavour'));
 console.log(require('./6-alternative-flavour').items)
 
-const name = require('./revision-1-7');
-require('./6-alternative-flavour');
-console.log(name.hello)
-console.log(require('./6-alternative-flavour'));
-console.log(require('./6-alternative-flavour').items)
-
-const name = require('./revision-1-7');
-require('./6-alternative-flavour');
-console.log(name.hello)
-console.log(require('./6-alternative-flavour'));
-console.log(require('./6-alternative-flavour').items)
